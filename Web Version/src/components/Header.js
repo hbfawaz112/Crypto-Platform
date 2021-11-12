da2ecd36d2d65012533fb963dffc5bbff538d7a2
Hussein B. Fawaz <hbfawaz112@gmail.com>
2021-11-12 18:28:31 +0200

    Added source code of the new version for web

---
+++ Web Version/src/components/Header.js
@@ -0,0 +1,63 @@
+import {
+  AppBar,
+  Container,
+  MenuItem,
+  Select,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import {
+  createTheme,
+  makeStyles,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+import { useHistory } from "react-router-dom";
+import { CryptoState } from "../CryptoContext";
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    flex: 1,
+    color: "gold",
+    fontFamily: "Montserrat",
+    fontWeight: "bold",
+    cursor: "pointer",
+  },
+}));
+
+const darkTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#fff",
+    },
+    type: "dark",
+  },
+});
+
+function Header() {
+  const classes = useStyles();
+  const { currency, setCurrency } = CryptoState();
+
+  const history = useHistory();
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color="transparent" position="static">
+        <Container>
+          <Toolbar>
+            <Typography
+              onClick={() => history.push(`/`)}
+              variant="h6"
+              className={classes.title}
+            >
+              Crypto Hunter
+            </Typography>
+            {/* <Button color="inherit">Login</Button> */}
+          
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  );
+}
+
+export default Header;
